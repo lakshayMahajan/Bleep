6527ac4db88674af7f67f5b5d052a86f6bf7e79f
Lakshay Mahajan <67165004+lakshayMahajan@users.noreply.github.com>
2025-03-29 22:21:05 -0400

    initialized project

---
+++ Bleep/BleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  BleepApp.swift
+//  Bleep
+//
+//  Created by Lakshay Mahajan on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct BleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bleep/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Bleep
+//
+//  Created by Lakshay Mahajan on 3/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
